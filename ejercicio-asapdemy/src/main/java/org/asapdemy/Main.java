407dcda66fbdf83d4af48d95eddbe5e0118a9e0f
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-05 17:39:26 -0300

    main con valores hardcodeados para pruebas

--- ejercicio-asapdemy/src/main/java/org/asapdemy/Main.java
+++ ejercicio-asapdemy/src/main/java/org/asapdemy/Main.java
@@ -1,17 +1,25 @@
 package org.asapdemy;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Sitio sitio = new Sitio("AsapDemy");
+        Categoria categoria = new Categoria("1","cocina");
+        Usuario autor = new Usuario ("u1" , "juan", "juangmail", false);
+        Curso curso = new Curso ("c1", "curso java", 100.0, 5 ,autor);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        categoria.agregarCurso(curso);
+
+        sitio.agregarCategoria(categoria);
+
+        //TODO:Probar con los enums de Resultado
+        boolean existe = sitio.existeCurso("c1");
+        System.out.println("existe el curso c1= " + existe);
+
+        boolean noExiste = sitio.existeCurso("c999");
+        System.out.println("existe el curso c999?= " + noExiste);
+
+        Resultado respuesta = sitio.suscribirseACurso(autor.getId(), curso.getId());
+        System.out.println("Esta es la respuesta: " + respuesta);
+        //TODO: Tomar los resultados de los enums y dejar mensajes claros. USUARIO_INEX ---> USUARIO INEXISTENTE
     }
-}
+ }
