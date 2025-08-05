6d647f941c154c5dec2d4af27bf22917618cbded
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-05 17:35:09 -0300

    usuario con getters

--- ejercicio-asapdemy/src/main/java/org/asapdemy/Usuario.java
+++ ejercicio-asapdemy/src/main/java/org/asapdemy/Usuario.java
@@ -2,12 +2,30 @@ package org.asapdemy;
 
 public class Usuario {
    private String id;
+   private String nombre;
    private String email;
    private boolean esBecado;
 
-    public Usuario(String id, String email, boolean esBecado) {
+    public Usuario(String id, String nombre, String email, boolean esBecado) {
         this.id = id;
+        this.nombre = nombre;
         this.email = email;
         this.esBecado = esBecado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isEsBecado() {
+        return esBecado;
     }
 }
