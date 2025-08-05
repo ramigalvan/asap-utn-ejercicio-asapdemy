8817f0f8d33467dc26405857f0ba1ee893050481
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-05 17:38:44 -0300

    sitio con funcionalidades suscribirseACurso, existeUsuario, existeCurso, agregarCategoria

--- ejercicio-asapdemy/src/main/java/org/asapdemy/Sitio.java
+++ ejercicio-asapdemy/src/main/java/org/asapdemy/Sitio.java
@@ -15,6 +15,52 @@ public class Sitio {
     }
 
     public Resultado suscribirseACurso(String usuarioId, String cursoId) {
+        //boolean cursoExistente = existeCurso(cursoId);
+        //boolean usuarioExistente = existeUsuario(usuarioId);
+        //TODO: hacer la logica de registrar al usuario al curso
+
+        if(!existeCurso(cursoId)){
+            return Resultado.CURSO_INEX;
+        }
+        if (!existeUsuario(usuarioId)) {
+            return Resultado.USUARIO_INEX;
+        }
         return null;
     }
+
+    public boolean existeUsuario(String usuarioId) {
+        boolean existeUsuario = false;
+        int i = 0;
+        while (i < usuarios.size() && !existeUsuario) {
+            if (usuarios.get(i).getNombre().equals(usuarioId)) {
+                existeUsuario = true;
+            }
+            i++;
+        }
+        return existeUsuario;
+    }
+
+    public boolean existeCurso(String cursoId) {
+        boolean existeCurso = false;
+        //categorias tienen cursos
+        //cada categoria tiene muchos cursos
+
+        int i = 0;
+        while (i < categorias.size() && !existeCurso) {
+            if (categorias.get(i).tieneCurso(cursoId)) {
+                existeCurso = true;
+            }
+            i++;
+        }
+        return existeCurso;
+    }
+
+    public void agregarCategoria(Categoria categoria){
+        categorias.add(categoria);
+    }
+
+
+    //agregar usuarios? agregarUsuario()¿
+    //agregar cursos? agregarCurso()
+
 }
