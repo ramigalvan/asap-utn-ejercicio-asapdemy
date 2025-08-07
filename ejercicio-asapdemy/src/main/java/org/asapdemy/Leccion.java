2ecc89a9bb3266290362f302c4bd5271f8e3eb0e
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-07 11:08:50 -0300

    Leccion metodos getters y setters, metodo agregado toString

--- ejercicio-asapdemy/src/main/java/org/asapdemy/Leccion.java
+++ ejercicio-asapdemy/src/main/java/org/asapdemy/Leccion.java
@@ -11,31 +11,6 @@ public class Leccion {
         this.tipo = tipo;
     }
 
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getDuracionMinutos() {
-        return duracionMinutos;
-    }
-
-    public void setDuracionMinutos(int duracionMinutos) {
-        this.duracionMinutos = duracionMinutos;
-    }
-
-    public TipoLeccion getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoLeccion tipo) {
-        this.tipo = tipo;
-    }
-
     @Override
     public String toString() {
         return "Leccion{" +
