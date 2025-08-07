4d2a3f764043365e86a231fd5695d26f1fd7e114
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-07 11:03:14 -0300

    agregando comentarios, getId, toString

--- ejercicio-asapdemy/src/main/java/org/asapdemy/Categoria.java
+++ ejercicio-asapdemy/src/main/java/org/asapdemy/Categoria.java
@@ -3,6 +3,7 @@ package org.asapdemy;
 import java.util.ArrayList;
 import java.util.List;
 
+//verificar que no se agreguen los cursos tengan un ID unico
 public class Categoria {
     private String id;
     private String nombre;
@@ -13,6 +14,12 @@ public class Categoria {
         this.nombre = nombre;
         this.cursos = new ArrayList<>();
     }
+
+    /**
+     * verifica si existe o no un curso
+     * @param cursoId
+     * @return
+     */
     public boolean tieneCurso(String cursoId) {
         boolean encontrado = false;
         int i = 0;
@@ -24,18 +31,38 @@ public class Categoria {
         }
         return encontrado;
     }
+
+
     public void agregarCurso(Curso curso){
         cursos.add(curso);
     }
 
-
     public Curso buscarCursoPorId(String cursoId) {
         Curso cursoBuscado = null;
-        for(Curso curso : cursos){
-            if(curso.getId() == cursoId){
-                cursoBuscado = curso;
+        int i = 0;
+        boolean encontrado = false;
+
+        while (i < cursos.size() && !encontrado) {
+            if (cursos.get(i).getId().equals(cursoId)) {
+                cursoBuscado = cursos.get(i);
+                encontrado = true;
             }
+            i++;
         }
+
         return cursoBuscado;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "nombre='" + nombre + '\'' +
+                ", id='" + id + '\'' +
+                '}';
     }
 }
