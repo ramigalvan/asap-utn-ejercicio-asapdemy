f7883e4dfd5c015bb81dde12984bb329531a1349
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-08-07 12:07:43 -0300

    Main modificado para cumplir todos los resultados

--- ejercicio-asapdemy/src/main/java/org/asapdemy/Main.java
+++ ejercicio-asapdemy/src/main/java/org/asapdemy/Main.java
@@ -1,13 +1,21 @@
 package org.asapdemy;
 
+
+
 public class Main {
     public static void main(String[] args) {
+        //creacion del sitio
         Sitio sitio = new Sitio("AsapDemy");
-        Categoria categoria = new Categoria("1","cocina");
-        Usuario autor = new Usuario ("u1" , "juan", "juangmail", false);
-        Usuario usuario1 = new Usuario ("u2" , "pedro", "pedormail", true);
-        Usuario usuario2 = new Usuario ("u3" , "jose", "josemail", false);
 
+        //creacion de las categorias
+        Categoria categoria = new Categoria("1","programacion");
+        Categoria categoria1 = new Categoria("2","cine");
+        Categoria categoria2 = new Categoria("2","cocina");
+
+        //creacion de los usuarios
+        Usuario autor = new Usuario ("9" , "juan", "juangmail", false);
+        Usuario usuario1 = new Usuario ("u1" , "pedro", "pedormail", true);
+        Usuario usuario2 = new Usuario ("u2" , "jose", "josemail", false);
         Usuario usuario3 = new Usuario("u3", "Ana", "ana@mail.com", true);
         Usuario usuario6 = new Usuario("u6", "Luis", "luis@mail.com", true);
         Usuario usuario7 = new Usuario("u7", "Jose", "jose@mail.com", true);
@@ -16,14 +24,15 @@ public class Main {
         Usuario usuario8 = new Usuario("u8", "Pedro", "pedro@mail.com", true);
         Usuario usuarioCorrecto = new Usuario("u9", "Perfect User", "perfectuser@mail.com", false);
 
-
         Curso curso = new Curso ("c1", "curso java", 100.0, 5 ,autor);
+        Curso curso1 = new Curso ("c2", "curso python", 200.0, 3 ,autor);
+        Curso curso2 = new Curso ("c2", "curso react", 150.0, 3 ,autor);
 
-        //primero agregalo al sitio ---> despues las operaciones restantes
-        categoria.agregarCurso(curso);
+
+        sitio.agregarCategoria(categoria);
+        sitio.agregarUsuario(autor);
         sitio.agregarUsuario(usuario1);
         sitio.agregarUsuario(usuario2);
-        sitio.agregarUsuario(autor);
         sitio.agregarUsuario(usuario3);
         sitio.agregarUsuario(usuario4);
         sitio.agregarUsuario(usuario5);
@@ -32,34 +41,50 @@ public class Main {
         sitio.agregarUsuario(usuario8);
         sitio.agregarUsuario(usuarioCorrecto);
 
-        sitio.agregarCategoria(categoria);
+        categoria.agregarCurso(curso);
 
-        //TODO: relacionar sitio, cateogira y curso --> categoria.agregarCurso();
-        //TODO: si el usuario no existe en el sitio, no debes de agregarlo al curso
-        curso.agregarUsuario(usuario1);
-        curso.agregarUsuario(usuario2);
-        curso.agregarUsuario(usuario3);
-        curso.agregarUsuario(usuario4);
-        curso.agregarUsuario(usuario5);
-        curso.agregarUsuario(usuario6);
-        curso.agregarUsuario(usuario7);
-        //curso.agregarUsuario(usuario8);
-        //NO AGREGO AL USUARIO PERFECTO
-
-
-        //TODO:Probar con los enums de Resultado
-        boolean existe = sitio.existeCurso("c1");
-        System.out.println("existe el curso c1= " + existe);
-
-        //boolean noExiste = sitio.existeCurso("c999");
-        //System.out.println("existe el curso c999?= " + noExiste);
-
-        //deberia de devolver el Resultado USUARIO YA SUSCRIPTO
-        Resultado respuesta = sitio.suscribirseACurso(usuarioCorrecto.getId(), curso.getId());
-
-
+        Resultado respuesta = null;
+        //SUSCRIPTO_OK("Usuario suscripto correctamente al curso");
+        respuesta = sitio.suscribirseACurso(usuario1.getId(), curso.getId());
         System.out.println(respuesta.getMensaje());
-        //TODO: Tomar los resultados de los enums y dejar mensajes claros. USUARIO_INEX ---> USUARIO INEXISTENTE
+
+        //YA_SUSCRIPTO("El usuario ya esta suscripto al curso"),
+        respuesta = sitio.suscribirseACurso(usuario1.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        //CURSO_INEX("El curso no existe"),
+        respuesta = sitio.suscribirseACurso(usuario1.getId(), "fh1013");
+        System.out.println(respuesta.getMensaje());
+
+        //USUARIO_INEX("El usuario no existe)"),
+        respuesta = sitio.suscribirseACurso("u456", curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        //ES_AUTOR("El autor del curso no puede suscribirse"),
+        respuesta = sitio.suscribirseACurso(autor.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        //MAX_BECADOS("Ya hay becados en el curso"),
+        respuesta = sitio.suscribirseACurso(usuario2.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        respuesta = sitio.suscribirseACurso(usuario3.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        respuesta = sitio.suscribirseACurso(usuario4.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        respuesta = sitio.suscribirseACurso(usuario5.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        respuesta = sitio.suscribirseACurso(usuario6.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        respuesta = sitio.suscribirseACurso(usuario7.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
+
+        respuesta = sitio.suscribirseACurso(usuario8.getId(), curso.getId());
+        System.out.println(respuesta.getMensaje());
 
 
     }
